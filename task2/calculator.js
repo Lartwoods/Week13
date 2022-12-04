fb831fdc939e9260e265f4029f114154db28ad33
Vlada <lartwoods@gmail.com>
2022-12-04 18:06:49 +0300

    added task2

---
+++ task2/calculator.js
@@ -0,0 +1,24 @@
+function addition() {
+  let a = prompt("Первое число?");
+  let b = prompt("Второе число?");
+
+  alert(Number(a) + Number(b));
+}
+function substraction() {
+  let a = prompt("Первое число?");
+  let b = prompt("Второе число?");
+
+  alert(Number(a) - Number(b));
+}
+function division() {
+  let a = prompt("Первое число?");
+  let b = prompt("Второе число?");
+
+  alert(Number(a) / Number(b));
+}
+function multiplication() {
+  let a = prompt("Первое число?");
+  let b = prompt("Второе число?");
+
+  alert(Number(a) * Number(b));
+}
